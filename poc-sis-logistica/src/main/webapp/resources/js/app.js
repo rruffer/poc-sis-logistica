19f9e2d30f84036841f3a02e33113eeec75e81cb
rruffer <alucard@22>
2018-10-06 23:21:41 -0300

    adicionando metodos para o menu

--- poc-sis-logistica/src/main/webapp/resources/js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	
-	$('.js-toggle').bind('click', function() {
-		$('.js-sidebar').toggleClass('is-toggled');
-		$('.js-content').toggleClass('is-toggled');
-	});
-	
-});
